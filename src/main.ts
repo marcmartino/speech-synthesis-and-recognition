19480981d879c2f9d1987ed3d9d28581facac76f
marcmartino <mrmarcmartino@gmail.com>
2018-01-28 16:01:43 -0800

    Added function for listening with multiple options

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import * as voices from "./modules/synthesisVoices";
 import { speak } from "./modules/synthesisSpeak";
-import { listenForWords } from "./modules/speechRecognition";
+import { listenForWords, listenWithOptions } from "./modules/speechRecognition";
 import * as transcripts from "./modules/flashcardTranscripts";
 import testCards from "./devAssets/testCards";
 
@@ -14,15 +14,25 @@ async function main() {
   const esListen = listenForWords("es-US");
   const enListen = listenForWords("en-US");
 
+  const esListenOpt = listenWithOptions('es-US');
+
   
   const promptCardDefs = transcripts.definition({nativeSpeak: enSpeak, learnedSpeak: esSpeak, nativeListen: enListen});
   const promptCardReverseDefs = transcripts.reverseDefinition({nativeSpeak: enSpeak, learnedSpeak: esSpeak, learnedListen: esListen});
 
-  promptCardReverseDefs(testCards[0])
+  /*promptCardReverseDefs(testCards[0])
     /*.then(() => promptCardDefs(testCards[1]))
     .then(() => promptCardReverseDefs(testCards[2]))
     .then(() => esSpeak('Has acabado con todo tus palabras.'))*/
-    .catch(() => console.log('cards failed'));
+    //.catch(() => console.log('cards failed'));
+
+    esSpeak('Bienvenido').then(() => 
+      esListenOpt([
+        {words: ['hola', 'alo', 'oye'], promise: () => esSpeak('hola chico')},
+        {words: ['horario', 'siguiente', 'adelante'], promise: () => esSpeak('otra cosas')}
+      ]))
+      .then(() => esSpeak('vámonos'))
+      .catch(() => esSpeak('mensaje de error'));
 
 }
 
--- src/modules/speechRecognition.ts
+++ src/modules/speechRecognition.ts
@@ -1,5 +1,6 @@
 import { removeAccents } from './languageTools';
 import { indexOf } from 'lodash';
+import { pluck, flatten, filter, contains, prop, compose, map, head } from 'ramda';
 // import { curry } from '@typed/curry';
 
 type LangShortCode = ('en-AU' | 'en-CA' | 'en-GH' | 'en-GB' | 'en-IN' | 'en-IE' | 'en-KE' | 'en-NZ' | 'en-NG' | 'en-PH' | 'en-ZA' | 'en-TZ' | 'en-US' | 'es-AR' | 'es-BO' | 'es-CL' | 'es-CO' | 'es-CR' | 'es-EC' | 'es-SV' | 'es-ES' | 'es-US' | 'es-GT' | 'es-HN' | 'es-MX' | 'es-NI' | 'es-PA' | 'es-PY' | 'es-PE' | 'es-PR' | 'es-DO' | 'es-UY' | 'es-VE');
@@ -36,6 +37,33 @@ const listenFor = (lang: LangShortCode = 'en-US') => (grammars: string[]) => (ex
 const listenForWords = (lang: LangShortCode) => (expectedWords: string[]):Promise<SpeechRecognitionEvent> =>
   listenFor(lang)([wordListToGrammar(expectedWords)])(expectedWords);
 
+const listenWithOptions = (lang: LangShortCode) => (expectedWordsAndPaths: WordListenRoute[]): Promise<SpeechRecognitionEvent> =>
+  listenForWords(lang)(flatten(pluck('words', expectedWordsAndPaths)))
+    .then(routeListenWordCondition(expectedWordsAndPaths));
+
+const routeListenWordCondition = (wordsAndPaths: WordListenRoute[]) => (speechEvent: SpeechRecognitionEvent): Promise<SpeechRecognitionEvent> =>
+  new Promise((resolve, reject) => {    
+    const filteredWord = filterRouteObj(speechEvent[0][0].transcript)(wordsAndPaths);
+
+    if (filteredWord) {
+      filteredWord.promise(speechEvent)
+        .then(resolve)
+        .catch(reject);
+    } else {
+      reject(speechEvent);
+    }
+  });
+
+const filterRouteObj = (word: string): (routingOptions: WordListenRoute[]) => WordListenRoute => 
+  head(
+    filter(
+      compose(
+        contains(removeAccents(word)),
+        map(removeAccents),
+        prop('words')),
+    )
+  );
+
 const wordListToGrammar = (words: string[]): string => `#JSGF V1.0;grammar Generic;public <Generic> = <generic>;<generic> = ( ${words.join(' | ')} );`;
 
 //TODO: should be lowercasing here
@@ -53,4 +81,9 @@ const genGrammarList = (grammars: string[]): SpeechGrammarList => {
   return grammarList;
 };
 
-export { listen, justListen, listenFor, listenForWords };
+export { listen, justListen, listenFor, listenForWords, listenWithOptions };
+
+interface WordListenRoute {
+    words: string[];
+    promise: (SpeechRecognitionEvent) => Promise<any>;
+}
